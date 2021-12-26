733adef4fd1208e3bd1b7cf720fd0da1cd9ce410
Misaka <3125016358@qq.com>
2021-12-26 12:33:55 +0800

    add key model and fix other model bug

--- models.py
+++ models.py
@@ -4,17 +4,26 @@ from flask import json
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.attributes import QueryableAttribute
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.sql import func
+from datetime import datetime
+
+import enum
+class UserTypeEnum(enum.Enum):
+    Admin = 1
+    Student = 2
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String(32), nullable=False)
+    type = db.Column(db.Enum(UserTypeEnum), nullable=False)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password,type):
         self.username = username
         self.set_password_hash(password)
-    
+        self.type = type
+
     def __repr__(self):
         return '<User %r>' % (self.username)
 
@@ -36,3 +45,18 @@ class Student(db.Model):
 
     def __repr__(self):
         return '<Student %r>' % (self.name)
+
+class Key(db.Model):
+    __tablename__ = 'keys'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    code = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.Boolean,default=False) # 激活码是否被使用
+    createDt = db.Column(db.DateTime,default=datetime.now())
+    activeDt = db.Column(db.DateTime)
+
+    def __init__(self,code):
+        self.code = code
+
+    def __repr__(self):
+        return '<Keys %r>' % (self.name)
+
