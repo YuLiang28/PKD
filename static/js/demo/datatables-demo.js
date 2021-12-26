a8801e589c5bd6670f49f28c066bac114085ccbe
Misaka <3125016358@qq.com>
2021-12-26 12:37:55 +0800

    disable databales demo js

--- static/js/demo/datatables-demo.js
+++ static/js/demo/datatables-demo.js
@@ -1,4 +1,14 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
-});
+// $(document).ready(function () {
+//   // 初始化 dataTable
+//   window.table = $('#dataTable').DataTable({
+//     language: {
+//       url: '/js/demo/datatables-zh.json' // 语言文件
+//     },
+//     stripeClasses: [], // 禁止添加 odd class
+//     select:{style: "single"} // 只允许选择单行
+//   });
+
+//     $('#delBtn').click(function () {
+//       table.row('.selected').remove().draw(false);// 点击删除选择行
+//     });
+// });
