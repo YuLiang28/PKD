820345e86882264a728a57b228d30a3a8a15011f
Misaka <3125016358@qq.com>
2023-01-31 16:01:50 +0800

    add notes

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlparse, urljoin
+
+# 检查 url 是否安全
 def is_safe_url(target,request):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
